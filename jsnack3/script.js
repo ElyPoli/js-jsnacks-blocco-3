ada0b54df5321eb33f80f93f9c8ff0ab0eeb16c8
ElyPoli <itsos.poli@gmail.com>
2023-07-07 16:21:06 +0200

    added js code - ex. Snack3

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,3 +2,39 @@
 Snack3 (Bonus)
 Fai inserire un numero, che chiameremo N, all’utente. Genera N array, ognuno formato da 10 numeri casuali da 1 a 100. Ogni volta che ne crei uno, stampalo.
 */
+
+// Dichiaro variabili
+const numberUser = parseInt(prompt("Inserisci il numero di liste che vuoi creare"));
+let random;
+const numberAdd = 10;
+let z = 0;
+
+// Controllo valore inserito dall'utente
+if (numberUser <= 0 || isNaN(numberUser)) {
+    alert("Si è verificato un errore");
+    location.reload();
+}
+
+console.log("Devo creare " + numberUser + " liste");
+
+// Genero il numero di liste chieste dall'utente
+for (let i = 1; i <= numberUser; i++) {
+    z = 0;
+    const listUser = [];
+
+    // Aggiungo ad ogni lista 10 numeri casuali
+    while (z < numberAdd) {
+        // Genero numeri casuali da 1 a 100
+        random = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
+        console.log(random);
+
+        // Aggiungo ogni numero generato alla lista
+        listUser.push(random);
+
+        // Aumento z
+        z++;
+    }
+
+    // Stampo la lista generata
+    console.log("Ecco la " + i + " lista generata: " + listUser);
+}
