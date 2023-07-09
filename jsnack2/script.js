f9bf59cd426c38f80c8a4f59e3481c92c04dfe75
ElyPoli <itsos.poli@gmail.com>
2023-07-09 18:27:49 +0200

    added user interaction - ex. Snack2

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,19 +6,39 @@ Crea due tag div con due id diversi. Un div avrà il testo colorato di rosso men
 // Dichiaro variabili
 const firstBox = document.getElementById("first-box");
 const secondBox = document.getElementById("second-box");
-const list = [15, 89, 64, 56, 23, 1, 8, 45, 20, 12, 36, 32, 29];
+const list = [];
+const totalList = document.querySelector(".total-list");
+const btnCreate = document.querySelector(".btn-create");
+const btnReset = document.querySelector(".btn-reset");
 
-// Scorro sulla lista per trovare i numeri dispari
-for (let i = 0; i < list.length; i++) {
+// Creo una lista con numeri randomici da 1 a 100
+for (let i = 0; i < 25; i++) {
+    list.push(Math.floor(Math.random() * (99 - 1 + 1)) + 1);
 
-    if (list[i] %  2 === 0){
-        // Il numero è pari
-        secondBox.classList.add("text-success");
-        secondBox.innerHTML += `<p>${list[i]}</p>`;
-
-    } else {
-        // Il numero è dispari
-        firstBox.classList.add("text-danger");
-        firstBox.innerHTML += `<p>${list[i]}</p>`;
-    }
+    // Mostro all'utente la lista creata
+    totalList.innerHTML += `<span>${list[i]} </span>`;
 }
+
+// Al click del pulsante la lista viene divisa in due
+btnCreate.addEventListener("click", function () {
+
+    // Scorro sulla lista per trovare i numeri dispari
+    for (let i = 0; i < list.length; i++) {
+
+        if (list[i] % 2 === 0) {
+            // Il numero è pari
+            secondBox.classList.add("text-success");
+            secondBox.innerHTML += `<li class="list-group-item">${list[i]}</li>`;
+        } else {
+            // Il numero è dispari
+            firstBox.classList.add("text-danger");
+            firstBox.innerHTML += `<li class="list-group-item">${list[i]}</li>`;
+        }
+    }
+})
+
+// Al click del pulsante la lista viene resettata
+btnReset.addEventListener("click", function () {
+    location.reload();
+})
+
